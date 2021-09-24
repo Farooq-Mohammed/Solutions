9dac97353c2009e6ccc41b076f050c18715aa8f6
staykirak <55940322+staykirak@users.noreply.github.com>
2021-09-24 19:19:36 +0530

    First commit

--- Easy/SetBits.java
+++ Easy/SetBits.java
@@ -1,21 +1,12 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
 class SetBits {
 	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
+		int n = 59;
 		//count set bits
 		int cnt = 0;
-		while(n>0){
-			cnt += (n%2==1 ? 1 : 0);
-			n /= 2;
+		while (n != 0) {
+		    n = n & (n - 1);
+		    cnt += 1;
 		}
 		System.out.println(cnt);
 	}
 }
-
-Link: https://ideone.com/dU24bz
